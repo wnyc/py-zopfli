5a7dda6ef009625649e964622644eca046489a2f
Adam DePrince <deprince@googlealumni.com>
2013-03-02 18:01:12 -0500

    fixes #1

--- zopfli/zlib.py
+++ zopfli/zlib.py
@@ -3,4 +3,5 @@ from __future__ import absolute_import
 import zopfli.zopfli
 
 def compress(data, *args, **kwargs):
-    return zopfli.zopfli.zlib(data)
+    kwargs['gzip_mode'] = 0
+    return zopfli.zopfli.compress(data, *args, **kwargs)
