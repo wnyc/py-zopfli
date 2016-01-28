f601ce000b08721765f8b0eb75b5bc2d1c801d6f
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-01-28 17:54:28 +0000

    [setup.py] change paths; remove unnecessary keywords; set version to 0.0.4

--- setup.py
+++ setup.py
@@ -10,34 +10,28 @@ from setuptools import setup, Extension
 
 setup(
     name='zopfli',
-    version='0.0.3',
+    version='0.0.4',
     author='Adam DePrince',
     author_email='adeprince@nypublicradio.org',
     description='Zopfli module for python',
     long_description=__doc__,
-    py_modules = [
-        'zopfli',
-        'zopfli.gzip',
-        'zopfli.zlib',
-        ],
     ext_modules = [Extension('zopfli.zopfli',
-                             opts = "-O2 -W -Wall -Wextra -ansi -pedantic -lm",
                              sources = [
-                                 'zopfli/blocksplitter.c',
-                                 'zopfli/cache.c',
-                                 'zopfli/deflate.c',
-                                 'zopfli/gzip_container.c',
-                                 'zopfli/squeeze.c',
-                                 'zopfli/hash.c',
-                                 'zopfli/katajainen.c',
-                                 'zopfli/lz77.c', 
-                                 'zopfli/tree.c',
-                                 'zopfli/util.c',
-                                 'zopfli/zlib_container.c',
-                                 'zopfli/zopflimodule.c',
+                                 'zopfli/src/zopfli/blocksplitter.c',
+                                 'zopfli/src/zopfli/cache.c',
+                                 'zopfli/src/zopfli/deflate.c',
+                                 'zopfli/src/zopfli/gzip_container.c',
+                                 'zopfli/src/zopfli/squeeze.c',
+                                 'zopfli/src/zopfli/hash.c',
+                                 'zopfli/src/zopfli/katajainen.c',
+                                 'zopfli/src/zopfli/lz77.c', 
+                                 'zopfli/src/zopfli/tree.c',
+                                 'zopfli/src/zopfli/util.c',
+                                 'zopfli/src/zopfli/zlib_container.c',
+                                 'src/zopflimodule.c',
                 ],
-                             libraries = ['c']
                              )],
+    package_dir={"": "src"},
     packages = ["zopfli"],
     zip_safe=True,
     license='ASL',
@@ -48,10 +42,6 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: System :: Archiving :: Compression',
         ],
-    scripts = [
-        ],
     url = "https://github.com/wnyc/pyzopfli",
-    install_requires = [
-        ]
 )
 
